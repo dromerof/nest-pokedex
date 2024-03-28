379fff787cf8676be80d7000b3d7612b75cb423d
DanielRomero <dandres508@gmail.com>
2024-03-28 11:57:53 -0500

    Antes de insertar por lote

--- README.md
+++ README.md
@@ -5,13 +5,16 @@
 # Ejecutar en desarrollo
 
 1. Clonar el repositorio
+```
+git clone https://github.com/dromerof/nest-pokedex.git
+```
 2. Ejecutar 
 ``` 
 yarn install
 ``` 
 3. Tener Nest CLI instalado
 ``` 
-npm o-g @nestjs/cli
+npm i -g @nestjs/cli
 ``` 
 4. Levantar la base de datos
 ``` 
@@ -19,5 +22,7 @@ docker-compose up -d
 ``` 
 
 ## Stack usado  
+* TypeScript
+* NestJS
 * MongoDB
-* Nestjs
+* Docker
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 
 @Module({
@@ -13,7 +14,7 @@ import { CommonModule } from './common/common.module';
       rootPath: join(__dirname,"..","public"),
       }),
     MongooseModule.forRoot("mongodb://localhost:27017/nest-pokemon"), 
-    PokemonModule, CommonModule,
+    PokemonModule, CommonModule, SeedModule,
   ],
 })
 export class AppModule {}
